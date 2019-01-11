3328c5ca62c1287c215088696772fb6a041b8892
MunSeng <34547107+willnil@users.noreply.github.com>
2019-01-11 18:40:32 +0100

    Update move_base.py

--- cv_pipeline/scripts/move_base.py
+++ cv_pipeline/scripts/move_base.py
@@ -1,54 +1,58 @@
 #!/usr/bin/env python
 import rospy
 import sys
+import message_filters
+import roslib; roslib.load_manifest('cv_pipeline')
 from geometry_msgs.msg import Twist
 from cv_pipeline.msg import FloatList
+from collections import deque
 
-class move_base:
+class MoveBase:
+  
+	def __init__(self):
+		self.ds = None
+		self.di = None
+		self.counter = 0
+		self.di_list = deque(maxlen=64) # pointer
+		self.rate = rospy.Rate(1)
+		self.distance_ist_sub = rospy.Subscriber('/base_movement/distance', FloatList, self.distanceist_callback) #distance_IST
+		self.distance_soll_sub = rospy.Subscriber('/movement/base_value', FloatList, self.distancesoll_callback) #distance_SOLL
+		#publisher
+		self.base_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
 
-  def __init__(self):
-	#subsriber
-	distance_ist_sub = rospy.Subscriber('/base_movement/distance', FloatList, self.distanceist_callback) #distance_IST
-	distance_soll_sub = rospy.Subscriber('', FloatList, self.distancesoll_callback) #distance_SOLL
-	#publisher
-	pub = rospy.Publisher('cmd_vel', Twist)
+	def distancesoll_callback(self, data):
+		rospy.loginfo("SOLL: {}".format(data.elements))
+		self.ds = data.elements
+		self.movebase()
+
+	def distanceist_callback(self, data):
+		#rospy.wait_for_message('/movement/base_value', FloatList)
+		
+		rospy.loginfo("IST: {}".format(data.elements))
+		if data.elements != (0, 0):
+			self.di_list.appendleft(data.elements)
+		#self.movebase()
+		self.counter += 1
+
+	def movebase(self):
+		rospy.loginfo("soll: {}, ist: {}".format(self.ds, self.di))
+		twist = Twist()
+		# linear speed
+		twist.linear.x = -0.1
+		twist.linear.y = 0
+		twist.linear.z = 0
+		# angular speed
+		twist.angular.x = 0 
+		twist.angular.y = 0
+		twist.angular.z = 0
+		for i in range(10):
+			self.base_pub.publish(twist)
+			rospy.sleep(0.1)
+		
+		twist = Twist()
+		self.base_pub.publish(twist)
 
 if __name__=="__main__":
 	rospy.init_node('move_base', anonymous=True)
 	while not rospy.is_shutdown():
-        	move_base = move_base()
-        	rospy.spin()
-
-
-
-	x = 0
-	y = 0
-	th = 0
-
-	try:
-		while(1):
-			if key in moveBindings.keys():
-				x = moveBindings[key][0]
-				y = moveBindings[key][1]
-				th = moveBindings[key][2]
-			elif key in speedBindings.keys():
-				speed = speed * speedBindings[key][0]
-				turn = turn * speedBindings[key][1]
-	
-			else:
-				x = 0
-				y = 0
-				th = 0
-
-			twist = Twist()
-			twist.linear.x = x*speed 
-			twist.linear.y = y*speed 
-			twist.linear.z = 0
-
-			twist.angular.x = 0 
-			twist.angular.y = 0
-			twist.angular.z = th*turn
-			pub.publish(twist)
-
-	except:
-		print e
+		move_base = MoveBase()
