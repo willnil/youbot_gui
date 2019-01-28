2e801e0fd4f49b2f2db2098913c9e4de9f1351c9
MunSeng <34547107+willnil@users.noreply.github.com>
2019-01-28 17:02:04 +0100

    Update move_base.py

--- cv_pipeline/scripts/move_base.py
+++ cv_pipeline/scripts/move_base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 import sys
+import math
 #import message_filters
 import roslib; roslib.load_manifest('cv_pipeline')
 from geometry_msgs.msg import Twist
@@ -20,7 +21,7 @@ class MoveBase:
 		self.base_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
 		self.counter = 0
 		self.di_list = deque(maxlen=64) # pointer
-		self.rate = rospy.Rate(1)
+		self.rate = rospy.Rate(10)
 
 	def distancesoll_callback(self, data):
 		rospy.loginfo("SOLL: {}".format(data.elements))
@@ -29,27 +30,52 @@ class MoveBase:
 
 	def distanceist_callback(self, data):
 		#rospy.wait_for_message('/movement/base_value', FloatList)
-		rospy.loginfo("IST: {}".format(data.elements))
-		#di = data.elements
+		#rospy.loginfo("IST: {}".format(data.elements))
+		di = data.elements
 		#if data.elements != (0, 0):
 		#	self.di_list.appendleft(di)
 		#self.movebase()
 		self.counter += 1
 
 	def movebase(self, ds):
-		rospy.loginfo("soll: {}, ist: {}".format(ds, 0))
-		twist = Twist()
-		# linear speed
-		twist.linear.x = -0.01*ds[1] # [m/s]
-		twist.linear.y = -0.01*ds[0] # [m/s]
+		rospy.loginfo("soll: {}".format(ds))
 		twist.linear.z = 0
 		# angular speed
 		twist.angular.x = 0 
 		twist.angular.y = 0
 		twist.angular.z = 0
-		for i in range(10):
-			self.base_pub.publish(twist)
-			rospy.sleep(0.01) # 10*0.01 = 0.1 move for 0.1 second
+		if ((abs(ds[1]) < 21) and (abs(ds[0]) < 21)): # < 0,1 m/s
+			twist = Twist()
+			# linear speed
+			twist.linear.x = -0.01*ds[1] # [m/s]
+			twist.linear.y = -0.01*ds[0] # [m/s]
+			for i in range(10):
+				self.base_pub.publish(twist)
+				rospy.sleep(0.01) # 10*0.01 = 0.1 (move for 0.1 second)
+		elif ((abs(ds[1]) >= 21) and (abs(ds[0]) == 0)): # y = 0,1 m/s
+			twist = Twist()
+			# linear speed
+			twist.linear.x = -math.copysign(0.1, ds[1]) # [m/s]
+			twist.linear.y = 0 # [m/s]
+			for i in range(10):
+				self.base_pub.publish(twist)
+				rospy.sleep(abs(ds[1])/1000)
+		elif ((abs(ds[0]) >= 21) and (abs(ds[1]) == 0)): # x = 0,1 m/s
+			twist = Twist()
+			# linear speed
+			twist.linear.x = 0 # [m/s]
+			twist.linear.y = -math.copysign(0.1, ds[0]) # [m/s]
+			for i in range(10):
+				self.base_pub.publish(twist)
+				rospy.sleep(abs(ds[0])/1000)
+		else:
+			twist = Twist()
+			# linear speed
+			twist.linear.x = -0.001*ds[1] # [m/s]
+			twist.linear.y = -0.001*ds[0] # [m/s]
+			for i in range(10):
+				self.base_pub.publish(twist)
+				rospy.sleep(0.1) # 10*0.1 = 1 (move for 1 second)
 		
 		twist = Twist() # stop moving
 		self.base_pub.publish(twist)
@@ -66,3 +92,4 @@ def main(args):
 		
 if __name__=="__main__":
 	main(sys.argv)
+
